ac346f125b1aa0ac0de1744c7c90319f95fe5825
Vivien <naernon@icloud.com>
2018-08-28 18:14:56 +0200

    Initial Commit

---
+++ Fedimos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fedimos
+//
+//  Created by Vivien BERNARD on 28/08/2018.
+//  Copyright © 2018 Fedimos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
